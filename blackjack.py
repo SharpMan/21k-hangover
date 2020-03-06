2adf92f54d1cb670829696822c9362adf357d4e6
MrGravey <gmark@pdx.edu>
2020-03-05 21:43:40 -0800

    Added variables int num_of_players and bool dealt to BlackJack class. Added deal()->bool method to BlackJack class.

--- blackjack.py
+++ blackjack.py
@@ -38,8 +38,10 @@ class BlackJack():
   
   def __init__(self):
     self.deck = self.new_deck()
+    self.num_of_players = 2
     self.player_hand = []
     self.dealer_hand = []
+    self.dealt = False
 
   def new_deck(self) -> list:
     deck = DECK_OF_CARDS.copy()
@@ -50,5 +52,30 @@ class BlackJack():
 
     return new_deck
 
+  def deal(self) -> bool:
+    if(self.dealt): return False
+    
+    cards_dealt = 0
+    player_turn = 0
+    while(cards_dealt < 2*self.num_of_players ): 
+      if(len(self.deck) <= 0): self.deck = self.new_deck()
+      
+      # deal to player
+      if(player_turn < self.num_of_players-1): 
+        self.player_hand.append(self.deck.pop())
+        player_turn += 1
+      # deal to a dealer
+      else:
+        self.dealer_hand.append(self.deck.pop())
+        player_turn = 0
+
+      cards_dealt += 1
+
+    self.dealt = True
+    return True
+
 game = BlackJack()
 print(game.deck)
+game.deal()
+print(game.player_hand)
+print(game.dealer_hand)
